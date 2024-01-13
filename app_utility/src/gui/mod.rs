dc7875e68d5fadd6c5f56480dc61854123c7d7b2
Simone Bugni Duch <simonebugniduch.SBD@gmail.com>
2024-01-13 17:27:14 +0100

    Fix

--- app_utility/src/gui/mod.rs
+++ app_utility/src/gui/mod.rs
@@ -1,24 +1,69 @@
+use std::sync::mpsc::{Sender, Receiver, channel};
+
 use eframe::{NativeOptions, run_native, App, Frame};
-use eframe::egui::{Context};
+use eframe::egui::{Align, Context, TopBottomPanel, Layout};
+use image::DynamicImage;
 use screenshots::display_info;
+
+#[derive(PartialEq, Eq)]
+enum Action {
+    None,
+    Screenshot
+}
 
 struct AppUtility {
     display: Option<usize>,
+    timer: Option<usize>,
+    sender: Sender<DynamicImage>,
+    receiver: Receiver<DynamicImage>,
+    current_screen: Option<DynamicImage>,
+    action: Action,
 }
 
 impl AppUtility {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        let _context = cc.egui_ctx.clone();
+        let context = cc.egui_ctx.clone();
+        let (sender, receiver) = channel();
 
         AppUtility {
             display: Some(0),
+            timer: Some(0),
+            sender,
+            receiver,
+            current_screen: None,
+            action: Action::None
         }
+    }
+
+    fn top_panel_view(&mut self, ctx: &Context, frame: &mut Frame) {
+        TopBottomPanel::top("top panel").show(ctx, |ui| {
+            ui.with_layout(Layout::left_to_right(Align::Center), |ui| {
+                if self.action == Action::None {
+                    let screenshot_btn: eframe::egui::Response = ui.add(Button::new("➕ New")).on_hover_text("Take screenshot of selected display");
+                    if screenshot_btn.clicked() {
+                        self.action = Action::Screenshot;
+                    }
+                    
+            } else {
+                let cancel_btn = ui.add(Button::new("❌ Cancel")).on_hover_text("Cancel");
+                if cancel_btn.clicked() {
+                    self.action = Action::None;
+                }
+            }
+            });
+        });
     }
 }
 
 impl App for AppUtility {
-    fn update(&mut self, _ctx: &Context, _frame: &mut Frame) {
-        
+    fn update(&mut self, ctx: &Context, frame: &mut Frame) {
+        // match self.receiver.try_recv() {
+        //     Ok(screenshot) => {
+        //         self.current_screen = Some(screenshot);
+        //     },
+        //     Err(_) => {}
+        // };
+        self.top_panel_view(ctx, frame);
     }
 }
 
