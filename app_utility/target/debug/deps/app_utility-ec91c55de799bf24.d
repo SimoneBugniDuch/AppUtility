edb73fb78ce561aa93a1ed986f9f6c23521c14be
EvelinaBratuhina <eve.bratuhina@gmail.com>
2024-01-14 10:47:02 +0100

    n

---
+++ app_utility/target/debug/deps/app_utility-ec91c55de799bf24.d
@@ -0,0 +1,6 @@
+C:\Users\evebr\Desktop\Università\LM I Anno\PROGRAMMAZIONE DI SISTEMA\Malnati\AppUtility\app_utility\target\debug\deps\app_utility-ec91c55de799bf24.rmeta: src\main.rs src\gui\mod.rs
+
+C:\Users\evebr\Desktop\Università\LM I Anno\PROGRAMMAZIONE DI SISTEMA\Malnati\AppUtility\app_utility\target\debug\deps\app_utility-ec91c55de799bf24.d: src\main.rs src\gui\mod.rs
+
+src\main.rs:
+src\gui\mod.rs:
---
+++ app_utility/target/debug/deps/egui_winit-bb8874b9ee45eded.d
@@ -0,0 +1,7 @@
+C:\Users\evebr\Desktop\Università\LM I Anno\PROGRAMMAZIONE DI SISTEMA\Malnati\AppUtility\app_utility\target\debug\deps\egui_winit-bb8874b9ee45eded.rmeta: C:\Users\evebr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\egui-winit-0.25.0\src\lib.rs C:\Users\evebr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\egui-winit-0.25.0\src\clipboard.rs C:\Users\evebr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\egui-winit-0.25.0\src\window_settings.rs
+
+C:\Users\evebr\Desktop\Università\LM I Anno\PROGRAMMAZIONE DI SISTEMA\Malnati\AppUtility\app_utility\target\debug\deps\egui_winit-bb8874b9ee45eded.d: C:\Users\evebr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\egui-winit-0.25.0\src\lib.rs C:\Users\evebr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\egui-winit-0.25.0\src\clipboard.rs C:\Users\evebr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\egui-winit-0.25.0\src\window_settings.rs
+
+C:\Users\evebr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\egui-winit-0.25.0\src\lib.rs:
+C:\Users\evebr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\egui-winit-0.25.0\src\clipboard.rs:
+C:\Users\evebr\.cargo\registry\src\index.crates.io-6f17d22bba15001f\egui-winit-0.25.0\src\window_settings.rs:
